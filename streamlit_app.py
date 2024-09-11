8aeb60bbe4cba3df0853c656f682dd7eb41918a2
maxbatsiteweb <73015150+maxbatsiteweb@users.noreply.github.com>
2024-09-11 09:07:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,7 @@ def calculate_speed_safe(distance, minutes, seconds):
     return speed, total_seconds
 
 results = {}
-total_seconds = []
+total_seconds = {}
 speeds = []
 for distance_text, distance_meters in zip(distances_test.keys(), distances_test.values()):
             st.write(f"**{distance_text}**")
@@ -74,13 +74,13 @@ for distance_text, distance_meters in zip(distances_test.keys(), distances_test.
                         seconds = st.number_input(f"Secondes", min_value=0, max_value=59, value=0, key=f"{distance_meters}_seconds")
             results[distance_text] = (minutes, seconds)
             speed, seconds_test = calculate_speed_safe(distance_meters, minutes, seconds)
-            total_seconds.append(seconds_test)
-            speeds.append(speed) 
+            total_seconds[distance_text] = seconds_test
+            speeds[distance_text] = speed
 
 if 0 not in total_seconds:
             st.write(total_seconds)
             st.write(results["3600 mètres"])
-            cs = 1200 / (results["1200 mètres"] + (results["3600 mètres"] - (3600/1200)*results["3600 mètres"])/(3600/1200 - 1))
+            cs = 1200 / (total_seconds["1200 mètres"] + (total_seconds["3600 mètres"] - (3600/1200)*total_seconds["3600 mètres"])/(3600/1200 - 1))
             st.write(cs)
 else:
             st.warning("Insère des temps non nuls pour les deux tests.")
