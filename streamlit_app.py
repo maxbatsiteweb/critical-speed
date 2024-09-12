4628ff993556bfc47d66c359bf4a7f3e52cd5e74
maxbatsiteweb <73015150+maxbatsiteweb@users.noreply.github.com>
2024-09-12 22:16:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,7 +100,7 @@ if 0 not in list(total_seconds.values()):
                                     st.markdown("---")
                                     st.write(f"**% de Vitesse Critique pour ton entraînement**")
 
-                                    percentage = st.slider('Choisis un pourcentage', min_value=1, max_value=100, step=1)
+                                    percentage = st.slider('', min_value=50, max_value=100, step=1)
                                     percentage /= 100
 
                                     percent_cs = percentage * cs
